b5318abc7314a2cb49df6b19154cc68fb8f901e1
ahmadazarnia <azarbonyad.ahmad@golrang.com>
2025-03-06 10:00:56 +0330

    merge all project

--- StringMethodsExample/src/Main.java
+++ StringMethodsExample/src/Main.java
@@ -4,41 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-
-
-
-
-
-        /*String sentence = "Hello UniDev Students";
-
-        String[] arrWords = sentence.split(" ");
-
-        String longestWord = "";
-        for (int i = 0; i < arrWords.length; i++) {
-            if(arrWords[i].length() > longestWord.length()){
-                longestWord = arrWords[i];
-            }
-        }
-
-        print("longest word is : " + longestWord);*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         String text = " Hello UniDev Students,Hello ";
 
         char charAtResult = text.charAt(7);
@@ -100,32 +65,6 @@ public class Main {
 
         String joinResult = String.join(",", "Ali", "Arash", "Hamed");
         print("join result is : " + joinResult);
-
-
-
-
-
-        print("String.format(ex:1) : " + String.format("%s", "Hello"));
-        String name = "Ali";
-        print("String.format(ex:2) : " + String.format("Hello Dear %s", name));
-
-        print("String.format(ex:3) : " + String.format("I am %s, i have %d years old", name, 23));
-        print("String.format(ex:4) : " + String.format("Balance = %,d", 2500000));
-        print("String.format(ex:5) : " + String.format("Time is : %02d:%02d", 8,4));
-        print("String.format(ex:6) : " + String.format("Time is : %02d:%02d", 11,44));
-        print("String.format(ex:7) : " + String.format("%d/%02d/%02d", 1403,12,8));
-        print("String.format(ex:8) : " + String.format("%d/%02d/%02d %02d:%02d", 1403,12,8, 3,24));
-        print("String.format(ex:9) : " + String.format("Point = %.2f", 18.3446786));
-
-        float[] rates = {3.5f,5,4,2,5,5,4,4.5f};
-
-        float sum= 0;
-        for (int i = 0; i < rates.length; i++) {
-            sum += rates[i];
-        }
-
-        float rate = sum / rates.length;
-        print(String.format("⭐ %.1f", rate));
     }
 
     public static void print(String message) {
